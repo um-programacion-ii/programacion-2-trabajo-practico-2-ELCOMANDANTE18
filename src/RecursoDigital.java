874b6aaf7d4ba76bce68de509c2dad06b91bfc30
ELCOMANDANTE18 <vb.gimenez@alumno.um.edu.ar>
2025-04-22 23:53:54 +0000

    Fix: Resolve múltiples errores de compilación y refactorización de herencia

--- src/RecursoDigital.java
+++
@@ -1,9 +0,0 @@
-package src;
-
-public interface RecursoDigital {
-    public String getTitulo();
-    public String getId();
-    public CategoriaRecurso getCategoria();
-    public ServicioNotificaciones getServicioNotificaciones();
-    public void mostrarDetalles();
-}
